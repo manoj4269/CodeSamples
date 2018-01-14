641d24698c80e572353ad0496e2a5e6f8ecd54ec
Manoj Kumar Yelavarthy <myelava@ncsu.edu>
2018-01-14 13:27:53 -0500

    subsets.py
    Finding subsets of a given list using dfs

---
+++ subsets.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def subsets(self, nums):
+        def dfs(depth, start, valuelist):
+            res.append(valuelist)
+            if depth == len(nums): return 
+            for i in range(start, len(nums)):
+                dfs(depth+1, i+1, valuelist+[nums[i]])
+        res = []
+        nums.sort()
+        dfs(0,0,[])
+        return res
